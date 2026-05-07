5da38b8e54a671d2ac73a859f098152bbe8a1e3e
Bob Senoff <bobsenoff@gmail.com>
2026-05-07 15:01:03 -0500

    Fix silent drop of named LAMBDA / non-range defined names on round-trip

--- spec/unit/xlsx/xform/book/defined-name-xform.spec.js
+++ spec/unit/xlsx/xform/book/defined-name-xform.spec.js
@@ -39,8 +39,39 @@ const expectations = [
     },
     preparedModel: {name: 'foo', ranges: []},
     xml: '<definedName name="foo">"OFFSET($A$10;0;0;0;1)"</definedName>',
-    parsedModel: {name: 'foo', ranges: []},
+    parsedModel: {name: 'foo', ranges: [], formula: '"OFFSET($A$10;0;0;0;1)"'},
     tests: ['parse'],
+  },
+  {
+    title: 'Range on sheet name containing parentheses',
+    create() {
+      return new DefinedNameXform();
+    },
+    // Sheet names with '(' must NOT be misclassified as formula expressions.
+    preparedModel: {name: 'Foo', ranges: ["'Data (2026)'!$A$1"]},
+    xml: "<definedName name=\"Foo\">'Data (2026)'!$A$1</definedName>",
+    parsedModel: {name: 'Foo', ranges: ["'Data (2026)'!$A$1"]},
+    tests: ['render', 'renderIn', 'parse'],
+  },
+  {
+    title: 'Named LAMBDA expression',
+    create() {
+      return new DefinedNameXform();
+    },
+    preparedModel: {name: 'MyDouble', ranges: [], formula: 'LAMBDA(x,x*2)'},
+    xml: '<definedName name="MyDouble">LAMBDA(x,x*2)</definedName>',
+    parsedModel: {name: 'MyDouble', ranges: [], formula: 'LAMBDA(x,x*2)'},
+    tests: ['render', 'renderIn', 'parse'],
+  },
+  {
+    title: 'Named LAMBDA with multiple parameters',
+    create() {
+      return new DefinedNameXform();
+    },
+    preparedModel: {name: 'MySum', ranges: [], formula: 'LAMBDA(x,y,x+y)'},
+    xml: '<definedName name="MySum">LAMBDA(x,y,x+y)</definedName>',
+    parsedModel: {name: 'MySum', ranges: [], formula: 'LAMBDA(x,y,x+y)'},
+    tests: ['render', 'renderIn', 'parse'],
   },
 ];
 
